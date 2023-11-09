96c1256afd636749af53cc9240d582e2b409fd2b
Molham Al khodari <alkhodari@drherwig.de>
2023-11-09 17:34:06 +0100

    add UserModel, Helpers auth, controllers & routing

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 
+import router from './router';
+import mongoose from 'mongoose';
+
 const app = express();
 
 app.use(cors({
@@ -21,3 +24,10 @@ server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
 
+const MONGO_URL = 'mongodb+srv://admin:admin@rezeptenetzwerk.g7tdnsm.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
