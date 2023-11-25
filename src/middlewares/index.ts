694cbc46c737e025bbce78b34f7c279c08968721
Molham Al khodari <alkhodari@drherwig.de>
2023-11-25 12:36:04 +0100

    recipes middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from './../db/users';
+import { getRecipeById } from "./../db/recipes";
 
 export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
@@ -14,6 +15,28 @@ export const isOwner = async (req: express.Request, res: express.Response, next:
     }
 
     if (currentUserId.toString() != id) {
+      return res.sendStatus(403);
+    }
+
+    return next();
+
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
+export const isRecipesOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, 'identity._id') as string;
+    const recipesOwner = await getRecipeById(id);
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() != recipesOwner.createdBy._id.toString()) {
       return res.sendStatus(403);
     }
 
