d750f6423abc3d0dae9ecb5a26c16ba5daebbbd7
Molham Al khodari <alkhodari@drherwig.de>
2024-03-19 00:38:23 +0100

    [UPDATE] express Router

--- src/router/recipes.ts
+++ src/router/recipes.ts
@@ -2,15 +2,14 @@
 import express from "express";
 
 import { getAllRecipes, getAllOwnerRecipes, getRecipe, createNewRecipe, deleteRecipe, updateRecipe, getCategoryRecipes, likeRecipeById, saveRecipeById, getSavedRecipes, postCommentById, deleteCommentById } from "../controllers/recipes";
-import { isAuthenticated, isRecipesOwner } from "../middlewares";
 
 export default (router: express.Router) => {
   router.get('/recipes', getAllRecipes);
   router.get('/recipes/owner/:id', getAllOwnerRecipes);
   router.get('/recipes/:id', getRecipe);
   router.post('/recipes/create', createNewRecipe);
-  router.patch('/recipes/:id', isAuthenticated, isRecipesOwner, updateRecipe);
-  router.delete('/recipes/:id', deleteRecipe); // ich musste hier isAuthenticated & isRecipesOwner löschen weil das mit dem cookies nicht funktioniert was ich schade finde :/
+  router.patch('/recipes/:id', updateRecipe);
+  router.delete('/recipes/:id', deleteRecipe);
   router.get('/recipes/category/:category', getCategoryRecipes);
 
   router.post('/recipes/like/:id', likeRecipeById);
