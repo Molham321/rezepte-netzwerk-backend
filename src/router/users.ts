2974c9176a78f516d8aacbd7d3aec3e036ed17c1
anna-lisa1404 <anna-lisa.merkel@fh-erfurt.de>
2024-03-26 23:26:47 +0100

    removed isAuthenticated in getAllUsers

--- src/router/users.ts
+++ src/router/users.ts
@@ -5,7 +5,8 @@ import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/use
 import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
-  router.get('/users', isAuthenticated, getAllUsers);
+  // router.get('/users', isAuthenticated, getAllUsers);
+  router.get('/users', getAllUsers);
   router.get('/users/:id', getUser);
   router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
   // router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
